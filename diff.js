059be613c36c2d89297c19f3197f946edaf88aa4
zmicier99 <40485411+zmicier99@users.noreply.github.com>
2018-07-09 11:31:11 +0300

    diff.js
    const diff = (integer1, integer2) => {
      const circle = 360;
      let int1=integer2-integer1;
      let int2=circle-integer2-integer1;
      let result;
      
      if (int1<int2 && int2>0) 
      return result = int1;
      else return result = int2;
      
      return result;
      } 
      
      diff (0,45);

---
+++ diff.js
@@ -0,0 +1,14 @@
+const diff = (integer1, integer2) => {
+  const circle = 360;
+  let int1=integer2-integer1;
+  let int2=circle-integer2-integer1;
+  let result;
+  
+  if (int1<int2 && int2>0) 
+  return result = int1;
+  else return result = int2;
+  
+  return result;
+  } 
+  
+  diff (0,45);
