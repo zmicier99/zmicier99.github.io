97019ad238f8eead15c38fe907c00f317fe128e8
zmicier99 <40485411+zmicier99@users.noreply.github.com>
2018-07-17 15:35:19 +0300

    Update diff.js

--- diff.js
+++ diff.js
@@ -1,14 +1,25 @@
+// BEGIN (write your solution here) (write your solution here)
 const diff = (integer1, integer2) => {
   const circle = 360;
-  const int1=integer2-integer1;
-  const int2=circle-integer2-integer1;
   let result;
-  
-  if (int1<int2 && int2>0) 
-  return result = int1;
-  else return result = int2;
-  
-  return result;
-  } 
-  
-  diff (0,45);
+
+  if (integer1 <= integer2 && integer2 < 180 && integer1 < 180)
+    return result = integer2 - integer1;
+
+  if (integer1 < integer2 && integer2 >= 180 && integer1 < 180)
+    return result = circle - integer2 + integer1;
+
+  if (integer1 < integer2 && integer2 >= 180 && integer1 > 180)
+    return result = integer2 - integer1;
+
+
+  if (integer1 > integer2 && integer1 >= 180)
+    return result = circle - integer1 + integer2;
+
+  if (integer1 > integer2 && integer1 < 180)
+    return result = integer1 - integer2;
+
+}
+// END
+export default diff;
+
