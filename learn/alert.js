22b9121e251c18a0b635070315fce3e5b85b5c4a
zmicier99 <zmicier99@gmail.com>
2018-12-04 15:58:30 +0300

    Create new folder

---
+++ learn/alert.js
@@ -0,0 +1,49 @@
+/* const alertMessage = alert('Привет мир!');
+
+const ageQuestion = prompt('Сколько Вам лет?', 36);
+
+if (ageQuestion === null) {
+    alert('Вам хз сколько лет(((');
+} else {
+    alert('Вам ' + ageQuestion + ' лет');
+}
+
+
+let login = prompt('Введите логин', '');
+if (login === null) { alert('Вход отменен'); }
+else if (login === 'Админ') {
+    const password = prompt('Введите пароль', '');
+    if (password === null) { alert('Вход отменен'); }
+    else if (password === 'Черный властелин') {
+        alert('Добро пожаловать!');
+    } else { alert('Неправильный пароль'); }
+}
+else {
+    alert("Доступ запрещен");
+} */
+
+
+/* for (let a = 1; a <= 10; a++) {
+    if (a % 2 == 0) {
+        alert (a);
+    }
+} */
+
+/* let number = 0;
+while (true) {
+    number = prompt("Введите число", '0');
+    if (number === null) {
+        alert('Отмена');
+        number = 'Отменено пользователем'; break;
+    }
+    if (number > 100) break;
+}
+alert('Введенное число: ' + number); */
+
+/* let a = +prompt('a?',"");
+switch (a) {
+    case 0: alert (a); break;
+    case 1: alert (a); break;
+    case 2: 
+    case 3: alert ('2,3'); break;
+} */
